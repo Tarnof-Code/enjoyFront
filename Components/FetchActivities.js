1a1aa9a926613cd09fe0a005b4ae6971c32c7906
Tarnof <tarnof@hotmail.com>
2022-07-04 07:43:09 +0200

    modif

--- Components/FetchActivities.js
+++ Components/FetchActivities.js
@@ -108,143 +108,144 @@ export default function FetchActivities(props) {
                     photo: require("../assets/LogosGroupes/Crabe.jpg")
 
                 });
-
-                for (let j = 0; j < response2.values.length; j++) {
-                    temp.push({
-                        date: response2.values[j][0],
-                        group: response2.values[j][1],
-                        anim: response2.values[j][2],
-                        morning: response2.values[j][3],
-                        afternoon: response2.values[j][4],
-                        photo: require("../assets/LogosGroupes/Crabe.jpg")
-                    });
-                };
-
-                for (let k = 0; k < response3.values.length; k++) {
-                    temp.push({
-                        date: response3.values[k][0],
-                        group: response3.values[k][1],
-                        anim: response3.values[k][2],
-                        morning: response3.values[k][3],
-                        afternoon: response3.values[k][4],
-                        photo: require("../assets/LogosGroupes/Crabe.jpg")
-                    });
-                };
-
-                for (let l = 0; l < response4.values.length; l++) {
-                    temp.push({
-                        date: response4.values[l][0],
-                        group: response4.values[l][1],
-                        anim: response4.values[l][2],
-                        morning: response4.values[l][3],
-                        afternoon: response4.values[l][4],
-                        photo: require("../assets/LogosGroupes/Crabe.jpg")
-                    });
-                };
-
-                for (let m = 0; m < response5.values.length; m++) {
-                    temp.push({
-                        date: response5.values[m][0],
-                        group: response5.values[m][1],
-                        anim: response5.values[m][2],
-                        morning: response5.values[m][3],
-                        afternoon: response5.values[m][4],
-                        photo: require("../assets/LogosGroupes/Crabe.jpg")
-                    });
-                };
-
-                for (let n = 0; n < response6.values.length; n++) {
-                    temp.push({
-                        date: response6.values[n][0],
-                        group: response6.values[n][1],
-                        anim: response6.values[n][2],
-                        morning: response6.values[n][3],
-                        afternoon: response6.values[n][4],
-                        photo: require("../assets/LogosGroupes/Crabe.jpg")
-                    });
-                };
-
-                for (let o = 0; o < response7.values.length; o++) {
-                    temp.push({
-                        date: response7.values[o][0],
-                        group: response7.values[o][1],
-                        anim: response7.values[o][2],
-                        morning: response7.values[o][3],
-                        afternoon: response7.values[o][4],
-                        photo: require("../assets/LogosGroupes/Crabe.jpg")
-                    });
-                };
-
-                for (let p = 0; p < response8.values.length; p++) {
-                    temp.push({
-                        date: response8.values[p][0],
-                        group: response8.values[p][1],
-                        anim: response8.values[p][2],
-                        morning: response8.values[p][3],
-                        afternoon: response8.values[p][4],
-                        photo: require("../assets/LogosGroupes/Crabe.jpg")
-                    });
-                };
-
-                for (let q = 0; q < response9.values.length; q++) {
-                    temp.push({
-                        date: response9.values[q][0],
-                        group: response9.values[q][1],
-                        anim: response9.values[q][2],
-                        morning: response9.values[q][3],
-                        afternoon: response9.values[q][4],
-                        photo: require("../assets/LogosGroupes/Crabe.jpg")
-                    });
-                };
-
-                for (let r = 0; r < response10.values.length; r++) {
-                    temp.push({
-                        date: response10.values[r][0],
-                        group: response10.values[r][1],
-                        anim: response10.values[r][2],
-                        morning: response10.values[r][3],
-                        afternoon: response10.values[r][4],
-                        photo: require("../assets/LogosGroupes/Crabe.jpg")
-                    });
-                };
-
-                for (let s = 0; s < response11.values.length; s++) {
-                    temp.push({
-                        date: response11.values[s][0],
-                        group: response11.values[s][1],
-                        anim: response11.values[s][2],
-                        morning: response11.values[s][3],
-                        afternoon: response11.values[s][4],
-                        photo: require("../assets/LogosGroupes/Crabe.jpg")
-                    });
-                };
-
-                for (let t = 0; t < response12.values.length; t++) {
-                    temp.push({
-                        date: response12.values[t][0],
-                        group: response12.values[t][1],
-                        anim: response12.values[t][2],
-                        morning: response12.values[t][3],
-                        afternoon: response12.values[t][4],
-                        photo: require("../assets/LogosGroupes/Crabe.jpg")
-                    });
-                };
-
-                for (let u = 0; u < response13.values.length; u++) {
-                    temp.push({
-                        date: response13.values[u][0],
-                        group: response13.values[u][1],
-                        anim: response13.values[u][2],
-                        morning: response13.values[u][3],
-                        afternoon: response13.values[u][4],
-                        photo: require("../assets/LogosGroupes/Crabe.jpg")
-                    });
-                };
-
             }
+
+            for (let j = 0; j < response2.values.length; j++) {
+                temp.push({
+                    date: response2.values[j][0],
+                    group: response2.values[j][1],
+                    anim: response2.values[j][2],
+                    morning: response2.values[j][3],
+                    afternoon: response2.values[j][4],
+                    photo: require("../assets/LogosGroupes/Crabe.jpg")
+                });
+            };
+
+            for (let k = 0; k < response3.values.length; k++) {
+                temp.push({
+                    date: response3.values[k][0],
+                    group: response3.values[k][1],
+                    anim: response3.values[k][2],
+                    morning: response3.values[k][3],
+                    afternoon: response3.values[k][4],
+                    photo: require("../assets/LogosGroupes/Crabe.jpg")
+                });
+            };
+
+            for (let l = 0; l < response4.values.length; l++) {
+                temp.push({
+                    date: response4.values[l][0],
+                    group: response4.values[l][1],
+                    anim: response4.values[l][2],
+                    morning: response4.values[l][3],
+                    afternoon: response4.values[l][4],
+                    photo: require("../assets/LogosGroupes/Crabe.jpg")
+                });
+            };
+
+            for (let m = 0; m < response5.values.length; m++) {
+                temp.push({
+                    date: response5.values[m][0],
+                    group: response5.values[m][1],
+                    anim: response5.values[m][2],
+                    morning: response5.values[m][3],
+                    afternoon: response5.values[m][4],
+                    photo: require("../assets/LogosGroupes/Crabe.jpg")
+                });
+            };
+
+            for (let n = 0; n < response6.values.length; n++) {
+                temp.push({
+                    date: response6.values[n][0],
+                    group: response6.values[n][1],
+                    anim: response6.values[n][2],
+                    morning: response6.values[n][3],
+                    afternoon: response6.values[n][4],
+                    photo: require("../assets/LogosGroupes/Crabe.jpg")
+                });
+            };
+
+            for (let o = 0; o < response7.values.length; o++) {
+                temp.push({
+                    date: response7.values[o][0],
+                    group: response7.values[o][1],
+                    anim: response7.values[o][2],
+                    morning: response7.values[o][3],
+                    afternoon: response7.values[o][4],
+                    photo: require("../assets/LogosGroupes/Crabe.jpg")
+                });
+            };
+
+            for (let p = 0; p < response8.values.length; p++) {
+                temp.push({
+                    date: response8.values[p][0],
+                    group: response8.values[p][1],
+                    anim: response8.values[p][2],
+                    morning: response8.values[p][3],
+                    afternoon: response8.values[p][4],
+                    photo: require("../assets/LogosGroupes/Crabe.jpg")
+                });
+            };
+
+            for (let q = 0; q < response9.values.length; q++) {
+                temp.push({
+                    date: response9.values[q][0],
+                    group: response9.values[q][1],
+                    anim: response9.values[q][2],
+                    morning: response9.values[q][3],
+                    afternoon: response9.values[q][4],
+                    photo: require("../assets/LogosGroupes/Crabe.jpg")
+                });
+            };
+
+            for (let r = 0; r < response10.values.length; r++) {
+                temp.push({
+                    date: response10.values[r][0],
+                    group: response10.values[r][1],
+                    anim: response10.values[r][2],
+                    morning: response10.values[r][3],
+                    afternoon: response10.values[r][4],
+                    photo: require("../assets/LogosGroupes/Crabe.jpg")
+                });
+            };
+
+            for (let s = 0; s < response11.values.length; s++) {
+                temp.push({
+                    date: response11.values[s][0],
+                    group: response11.values[s][1],
+                    anim: response11.values[s][2],
+                    morning: response11.values[s][3],
+                    afternoon: response11.values[s][4],
+                    photo: require("../assets/LogosGroupes/Crabe.jpg")
+                });
+            };
+
+            for (let t = 0; t < response12.values.length; t++) {
+                temp.push({
+                    date: response12.values[t][0],
+                    group: response12.values[t][1],
+                    anim: response12.values[t][2],
+                    morning: response12.values[t][3],
+                    afternoon: response12.values[t][4],
+                    photo: require("../assets/LogosGroupes/Crabe.jpg")
+                });
+            };
+
+            for (let u = 0; u < response13.values.length; u++) {
+                temp.push({
+                    date: response13.values[u][0],
+                    group: response13.values[u][1],
+                    anim: response13.values[u][2],
+                    morning: response13.values[u][3],
+                    afternoon: response13.values[u][4],
+                    photo: require("../assets/LogosGroupes/Crabe.jpg")
+                });
+            };
             setLoading(false);
             setActiList(temp);
         }
+
+
 
         getActivities()
 
